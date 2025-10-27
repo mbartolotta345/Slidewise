d376c6e67c3a45507f4ef502a3250add8c9071f2
Michelle Bartolotta <michb@udel.edu>
2025-10-26 21:59:56 -0400

    initialized board length, height, and correct words

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import './App.css'
 
 function App() {
   
+  //initialize how big the board is
+  const BOARD_LENGTH = 4;
+  const BOARD_HEIGHT = 5;
+
+  //create a 2d matrix for the board
+
+  let board = Array.from({ length: BOARD_LENGTH }, () => Array(BOARD_HEIGHT).fill(0));
+
+  console.log(board);
+
+  //initialize where the barrier blocks are
+
+  //initialize letters in the board squares that are usuable
+
+  //initialize the correct words
+  const correct_words = ["GOLF","WORK"];
 
   return (
     <>
@@ -12,4 +28,18 @@ function App() {
   )
 }
 
-export default App
+function WordLine({word}) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function LetterBox() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
